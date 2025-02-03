e5bb17407c039f9a693a88b086d838aa1d49bd40
Nate Smith <nathan.l.smith89@gmail.com>
2025-02-03 16:34:26 +1000

    Add `Device.osName`

--- Sources/DetectorKit/Device.swift
+++ Sources/DetectorKit/Device.swift
@@ -1,5 +1,18 @@
 import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
 
 struct Device {
     static let systemVersion: String = ProcessInfo.processInfo.operatingSystemVersionString
+    @MainActor
+    static var osName: String {
+        #if os(macOS)
+            return "macOS"
+        #elseif canImport(UIKit)
+            return UIDevice.current.systemName
+        #else
+            return "-"
+        #endif
+    }
 }
--- Tests/DetectorKitTests/DeviceTests.swift
+++ Tests/DetectorKitTests/DeviceTests.swift
@@ -2,7 +2,15 @@ import Testing
 
 @testable import DetectorKit
 
-@Test func testDeviceDetector() async throws {
-    print("Device.systemVersion: \(Device.systemVersion)")
-    #expect(Device.systemVersion.contains("Version"))
+@Suite("Device info tests")
+struct DeviceTests {
+    @Test("can detect os name")
+    func testDetectOSName() async throws {
+        #expect(await Device.osName != "-")
+    }
+
+    @Test("can detect system version")
+    func testDetectSystemVersion() async throws {
+        #expect(Device.systemVersion.contains("Version"))
+    }
 }
