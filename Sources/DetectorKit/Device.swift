ba0e87f7ea344711a7fa23f400a07e8180e83576
Nate Smith <nathan.l.smith89@gmail.com>
2025-02-05 10:00:53 +1000

    Add `Device.model`

--- Sources/DetectorKit/Device.swift
+++ Sources/DetectorKit/Device.swift
@@ -3,16 +3,58 @@ import Foundation
 import UIKit
 #endif
 
+@MainActor
 struct Device {
-    static let systemVersion: String = ProcessInfo.processInfo.operatingSystemVersionString
-    @MainActor
+    static let osVersion: String = ProcessInfo.processInfo.operatingSystemVersionString
     static var osName: String {
         #if os(macOS)
-            return "macOS"
+        return "macOS"
+        #elseif os(watchOS)
+        return "watchOS"
         #elseif canImport(UIKit)
-            return UIDevice.current.systemName
+        return UIDevice.current.systemName
         #else
-            return "-"
+        return "-"
         #endif
     }
+    static var deviceModel: String {
+        #if os(macOS)
+        return sysctlString(name: "hw.model") ?? "-"
+        #elseif os(watchOS)
+        return sysctlString(name: "hw.model") ?? "-"
+        #elseif canImport(UIKit)
+        return UIDevice.current.model
+        #else
+        return "-"
+        #endif
+    }
+}
+
+extension Device {
+    // Function to get the value of a sysctl parameter
+    static func sysctlbyname<T>(name: String, type: T.Type) -> T? {
+        var size: Int = 0
+        Darwin.sysctlbyname(name, nil, &size, nil, 0)
+        let value = UnsafeMutableRawPointer.allocate(
+            byteCount: size,
+            alignment: MemoryLayout<T>.alignment
+        )
+        defer { value.deallocate() }
+        if Darwin.sysctlbyname(name, value, &size, nil, 0) == 0 {
+            return value.load(as: T.self)
+        }
+        return nil
+    }
+
+    // Function to get a string value from sysctl
+    static func sysctlString(name: String) -> String? {
+        var size: Int = 0
+        Darwin.sysctlbyname(name, nil, &size, nil, 0)
+        var value = [CChar](repeating: 0, count: size)
+        if Darwin.sysctlbyname(name, &value, &size, nil, 0) == 0 {
+            return String(utf8String: value)
+        }
+        return nil
+    }
+
 }
--- Tests/DetectorKitTests/DeviceTests.swift
+++ Tests/DetectorKitTests/DeviceTests.swift
@@ -6,11 +6,19 @@ import Testing
 struct DeviceTests {
     @Test("can detect os name")
     func testDetectOSName() async throws {
+        print(await Device.osName)
         #expect(await Device.osName != "-")
     }
 
     @Test("can detect system version")
     func testDetectSystemVersion() async throws {
-        #expect(Device.systemVersion.contains("Version"))
+        print(await Device.osVersion)
+        #expect(await Device.osVersion.contains("Version"))
+    }
+
+    @Test("can detect the device model")
+    func testDetectDeviceModel() async throws {
+        print(await Device.deviceModel)
+        #expect(await Device.deviceModel != "-")
     }
 }
